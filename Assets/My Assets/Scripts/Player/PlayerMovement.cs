c33ff26c82c9cb34ef532b7a6d96cb56daa64d13
DovydasJakuciunas <d00244777@student.dkit.ie>
2024-12-18 10:34:10 +0000

    🐛 🚧 Double Tap = Sprint
    Attempted to add double tap to sprint. It works but there's a slippery bug for movement

--- Assets/My Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/My Assets/Scripts/Player/PlayerMovement.cs
@@ -5,26 +5,43 @@ using UnityEngine.AI;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Camera cam;
-    public NavMeshAgent agent;
-    public GameObject tagetDest;
+    [SerializeField]private Camera cam;
+    [SerializeField]private NavMeshAgent player;
+    [SerializeField]private GameObject tagetDest;
+    [SerializeField]private LineRenderer line;
+
+    private float lastClickTime;
+    private const float DOUBLE_CLICK_TIME = 0.2f;
 
     private void Update()
     {
         if(Input.GetMouseButtonDown(0))
         {
+            float timeSinceLastClick = Time.time - lastClickTime;   //Gets the time since last click
+            lastClickTime = Time.time;                              //Gets last time mouse was clicked
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);    //Get camera and get the mouse position
             RaycastHit hitPoint;
 
             if(Physics.Raycast(ray, out hitPoint))                  //If the raycast hit something move the agent to that position
             {
                 tagetDest.transform.position = hitPoint.point;
-                agent.SetDestination(hitPoint.point);
+                player.SetDestination(hitPoint.point);
             }
-
+            if(timeSinceLastClick <= DOUBLE_CLICK_TIME)            //If the time since last click is less than the double click time
+            {
+                player.speed = player.speed * 2;      //Double the acceleration of agent
+                player.SetDestination(tagetDest.transform.position); //Move the agent to the target destination
+            }
         }
+        DrawPath();
     }
 
+    private void DrawPath()
+    {
+        if(player.path.corners.Length <2) return;
 
+        line.positionCount = player.path.corners.Length;
+        line.SetPositions(player.path.corners);
+    }
 
 }
